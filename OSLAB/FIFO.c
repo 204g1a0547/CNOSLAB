34ab0ce5b6f3c8d2b108cd54a7b8eb5dc2621f56
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-01 12:50:40 +0530

    R.Lalitha Created FIFO.c

---
+++ OSLAB/FIFO.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: R.Lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: page replacements algorithms.
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/

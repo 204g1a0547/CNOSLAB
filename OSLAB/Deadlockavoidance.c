a1f748233b05c8198f8e559390922016cd9e7958
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-08 11:47:09 +0530

    R.lalitha created Deadlockavoidance.c

---
+++ OSLAB/Deadlockavoidance.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: R.Lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment:Bankers Algorithm using DeadlockAvoidance
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
